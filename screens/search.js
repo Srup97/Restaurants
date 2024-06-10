06b755d49825a11800ede146c0caafcac702c134
Srup97 <srup_97@hotmail.com>
2024-06-09 23:17:44 -0400

    pantallas de navegacion

---
+++ screens/search.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function search() {
+  return (
+    <View>
+      <Text>search</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
